dacd8fdb2ba84cdcfb65fe9608b3938433c58529
zhaolei <zhaolei@thunk.cn>
2019-07-30 16:43:17 +0800

    Init Commit

--- web/src/config/index.js
+++ web/src/config/index.js
@@ -7,8 +7,7 @@ export default {
   /**
    * @description websocket服务端地址
    */
-  webSocketUrl: 'https://api.iot.thunk.cn/socket',
-  // webSocketUrl: 'http://127.0.0.1:2500/socket',
+  webSocketUrl: 'http://127.0.0.1:2500/socket',
 
   /**
    * @description 广播Topic
